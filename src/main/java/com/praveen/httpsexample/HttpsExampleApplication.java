94d5971538a9d1161d1e1d07b384bd77b81ba240
belkap <praveen.belkar@emc.com>
2019-03-18 11:09:47 +0530

    initial commit

---
+++ src/main/java/com/praveen/httpsexample/HttpsExampleApplication.java
@@ -0,0 +1,12 @@
+package com.praveen.httpsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsExampleApplication.class, args);
+	}
+}
